ba8536c884e8655963bd749283db77556609190a
ponfry <paschaotdel@gmail.com>
2016-05-13 17:46:59 +0300

    Осуществлено создание элемента

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 document.getElementsByClassName("create")[0].onclick = function () {
-    var englishword = document.getElementsByClassName("english-word")[0].value;
-    var russianword = document.getElementsByClassName("russian-word")[0].value;
-    var new_word = englishword + "-" + russianword;
-    var type_new_word;
+
+    var type_new_word = get_type();
+
+    document.getElementsByClassName("dictionary")[0].appendChild(create_new_element("span", "word", get_word));
+    
+    document.getElementsByClassName("dictionary")[0].appendChild(create_new_element("span", get_type(), get_part_of_speech));
+};
+function get_part_of_speech() {
+    var type = get_type();
+    if(type === "noun"){
+        return "сущ."
+    }
+    if(type === "adjective"){
+        return "прил."
+    }
+    if(type === "verb"){
+        return "глаг."
+    }
+    if(type === "pronoun"){
+        return "местоим."
+    }
+    if(type === "adverb"){
+        return "нар."
+    }
+
+}
+function get_word() {
+    var english_word = document.getElementsByClassName("english-word")[0].value;
+    var russian_word = document.getElementsByClassName("russian-word")[0].value;
+    return english_word + "-" + russian_word;
+}
+function get_type() {
     var types = document.getElementsByName("gender");
-    for (var i=0;i<types.length; i++) {
+    for (var i = 0; i < types.length; i++) {
         if (types[i].checked) {
-            type_new_word = types[i].value;
+            return types[i].value;
         }
     }
-    new_word=new_word+type_new_word;
-    var new_element = document.createElement("span");
-    new_element.className = "word";
-    new_element.innerHTML = new_word;
-    document.getElementsByClassName("dictionary")[0].appendChild(new_element);
-};
+
+}
+function create_new_element(type, class_name, action) {
+    var new_element= document.createElement(type);
+    new_element.className = class_name;
+    new_element.innerHTML = action();
+    return new_element;
+}
