f34a84942adbc9039aa37df36fb657d92f6765d2
ponfry <paschaotdel@gmail.com>
2016-05-13 15:28:32 +0300

    Осущуствлен вывод слов и их части речи

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ document.getElementsByClassName("create")[0].onclick = function () {
     var englishword = document.getElementsByClassName("english-word")[0].value;
     var russianword = document.getElementsByClassName("russian-word")[0].value;
     var new_word = englishword + "-" + russianword;
-    var new_element = document.createElement("label");
+    var type_new_word;
+    var types = document.getElementsByName("gender");
+    for (var i=0;i<types.length; i++) {
+        if (types[i].checked) {
+            type_new_word = types[i].value;
+        }
+    }
+    new_word=new_word+type_new_word;
+    var new_element = document.createElement("span");
     new_element.className = "word";
     new_element.innerHTML = new_word;
     document.getElementsByClassName("dictionary")[0].appendChild(new_element);
