f147fdd178326f4f49c6285c31c759ddc10adeb0
ponfry <paschaotdel@gmail.com>
2016-05-17 14:58:19 +0300

    Новый заголовок

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 document.getElementsByClassName("create")[0].onclick = function () {
 
-    document.getElementsByClassName("dictionary")[0].appendChild(create_new_element("span", "eword", get_english_word));
-    document.getElementsByClassName("dictionary")[0].appendChild(create_new_element("span", "rword", get_russian_word));
-    document.getElementsByClassName("dictionary")[0].appendChild(create_new_element("span", get_type(), get_part_of_speech));
+    document.getElementsByClassName("dictionary")[0].appendChild(createNewElement("span", "eword", getEnglishWord));
+    document.getElementsByClassName("dictionary")[0].appendChild(createNewElement("span", "rword", getRussianWord));
+    document.getElementsByClassName("dictionary")[0].appendChild(createNewElement("span", getType(), getPartOfSpeech));
 };
-function get_part_of_speech() {
-    var type = get_type();
-    if(type === "noun"){
-        return "сущ."+"<br>"
-    }
-    if(type === "adjective"){
-        return "прил."+"<br>"
-    }
-    if(type === "verb"){
-        return "глаг."+"<br>"
-    }
-    if(type === "pronoun"){
-        return "местоим."+"<br>"
-    }
-    if(type === "adverb"){
-        return "нар."+"<br>"
-    }
+
+function getPartOfSpeech() {
+    // var type = getType();
+    // if(type === "noun"){
+    //     return "сущ."+"<br>"
+    // }
+    // if(type === "adjective"){
+    //     return "прил."+"<br>"
+    // }
+    // if(type === "verb"){
+    //     return "глаг."+"<br>"
+    // }
+    // if(type === "pronoun"){
+    //     return "местоим."+"<br>"
+    // }
+    // if(type === "adverb"){
+    //     return "нар."+"<br>"
+    // }
+    
+    return TYPES[type];
 
 }
-function get_english_word() {
+
+function getEnglishWord() {
    return document.getElementsByClassName("english-word")[0].value;
 }
-function get_russian_word() {
+
+function getRussianWord() {
     return document.getElementsByClassName("russian-word")[0].value;
 }
-function get_type() {
+
+function getType() {
     var types = document.getElementsByName("gender");
     for (var i = 0; i < types.length; i++) {
         if (types[i].checked) {
             return types[i].value;
         }
     }
+}
 
+function createNewElement(type, className, action) {
+    var newElement= document.createElement(type);
+    newElement.className = className;
+    newElement.innerHTML = action();
+
+    return newElement;
 }
-function create_new_element(type, class_name, action) {
-    var new_element= document.createElement(type);
-    new_element.className = class_name;
-    new_element.innerHTML = action();
-    return new_element;
-}
+
+var TYPES = {
+    noun: "сущ.\n"
+};
